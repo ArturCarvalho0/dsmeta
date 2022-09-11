52794658c9f8e06ac0f84ba32256a1914fff0dc2
Artur Carvalho <artur.carvalho0@outlook.com>
2022-09-11 09:15:47 -0300

    Project created

---
+++ backend/src/main/java/com/ArturCarvalho0/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.ArturCarvalho0.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
