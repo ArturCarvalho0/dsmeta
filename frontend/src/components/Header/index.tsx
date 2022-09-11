5cfe017b146ff3eeb0814ae310d92b5e6820cb22
Artur Carvalho <artur.carvalho0@outlook.com>
2022-09-11 14:08:29 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/ArturCarvalho0">@ArturCarvalho0</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
