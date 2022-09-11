6b9db4a47fe48b20e44541fe53bf2fac9714d2e0
Artur Carvalho <artur.carvalho0@outlook.com>
2022-09-11 17:43:44 -0300

    Date select

--- backend/src/main/java/com/ArturCarvalho0/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/ArturCarvalho0/dsmeta/controllers/SaleController.java
@@ -3,8 +3,11 @@ package com.ArturCarvalho0.dsmeta.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ArturCarvalho0.dsmeta.entities.Sale;
@@ -13,12 +16,15 @@ import com.ArturCarvalho0.dsmeta.services.SaleService;
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
-	
+
 	@Autowired
 	private SaleService service;
-	
+
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
 }
