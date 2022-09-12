50f20f00fc76358ec5b74d7aa5c6a1dd661814b1
Artur Carvalho <artur.carvalho0@outlook.com>
2022-09-11 21:23:47 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,8 +21,6 @@ function SalesCard() {
     const dmin = minDate.toISOString().slice(0,10);
     const dmax = maxDate.toISOString().slice(0,10);
 
-    console.log(dmin);
-
     axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
       setSales(response.data.content);
     });
@@ -74,7 +72,7 @@ function SalesCard() {
                   <td>{sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
